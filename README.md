0370d597547222b66bae3011ee748b068a6bf87e
Rory Linehan <rory.linehan@agoradata.com>
2022-11-09 12:41:40 -0600

    update readme, add multiple curl calls

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This is a self-contained proof of concept implemented using [this page](https://
 ## Usage
 
 To run the automatic instrumentation demo: `./run.sh auto`, and the manual demo: `./run.sh manual`.
+
+When the logs are shown, the traces will be displayed immediately. You'll need to wait for about 60 seconds for telemetry metrics to come through.
--- run.sh
+++ run.sh
@@ -4,8 +4,12 @@ if [ "auto" = $mode ]; then
     docker build -t otel-poc-automatic:latest -f Dockerfile.automatic . && \
     echo "starting container..." && docker run -d --rm --name otel-auto otel-poc-automatic:latest && \
     sleep 3 && \
-    echo "rolling dice..." && docker exec -it otel-auto curl http://127.0.0.1:5000/rolldice && echo && \
-    echo "telemetry logs (ctrl+c after telemetry info is displayed):" && docker logs --follow otel-auto
+    echo "rolling dice..."
+    for count in 1 2 3 4 5
+    do
+        docker exec -it otel-auto curl http://127.0.0.1:5000/rolldice && echo
+    done
+    echo "telemetry logs (ctrl+c to exit):" && sleep 3 && docker logs --follow otel-auto
     docker stop otel-auto
 fi
 #docker build -t otel-poc-manual:latest -f Dockerfile.manual .
